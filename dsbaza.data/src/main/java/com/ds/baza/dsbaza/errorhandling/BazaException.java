864f127f5d8398215c674b91f90fa8b4508f9b61
yx25cv <42892130+yx25cv@users.noreply.github.com>
2019-01-07 10:45:10 +0100

    Second

--- dsbaza.data/src/main/java/com/ds/baza/dsbaza/errorhandling/BazaException.java
+++ dsbaza.data/src/main/java/com/ds/baza/dsbaza/errorhandling/BazaException.java
@@ -1,8 +1,5 @@
 package com.ds.baza.dsbaza.errorhandling;
 
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-
 public class BazaException extends RuntimeException{
 
     int statusCode;
@@ -65,11 +62,8 @@ public class BazaException extends RuntimeException{
     public static Pair<Integer, String> THERE_IS_NO_RESULT = new Pair<Integer, String>(109, "Nije pronadjen nijedan rezultat!");
     public static Pair<Integer, String> INVALID_DATE = new Pair<Integer, String>(110, "Datum mora biti unet u sledecem formatu DD.MM.YYYY.");
     public static Pair<Integer, String> NASLOV = new Pair<Integer, String>(102, "Naslov nije dobar");
-    public static Pair<Integer, String> KRATAK_SADRZAJ = new Pair<Integer, String>(103, "Kratak sadrzaj nije dobar");
-    public static Pair<Integer, String> SADRZAJ = new Pair<Integer, String>(104, "Sadrzaj nije dobar");
-    public static Pair<Integer, String> CATEGORY = new Pair<Integer, String>(105, "Kategorija nije dobra");
     public static Pair<Integer, String> PROBLEM_SA_BAZOM = new Pair<Integer, String>(106, "Nije uspeo upis u bazu");
-    public static Pair<Integer, String> ALREADY_EXIST = new Pair<Integer, String>(107, "Vec postoji objekat u bazi sa istim imenom");
+    public static Pair<Integer, String> ALREADY_EXIST = new Pair<Integer, String>(107, "Vec postoji objekat u bazi sa istim JMBG");
     public static Pair<Integer, String> ID_DOESNT_EXIST = new Pair<Integer, String>(108, "ID nije pronadjen");
 
 
